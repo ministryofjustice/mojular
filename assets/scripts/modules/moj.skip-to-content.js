cb63a775dfc653d7d605f6355ca8bb082e265a9c
Tyom Semonov <inspiro@gmail.com>
2015-08-26 16:57:39 +0100

    Add behaviour for skip to content link to focus on #content

---
+++ assets/scripts/modules/moj.skip-to-content.js
@@ -0,0 +1,18 @@
+(function () {
+  'use strict';
+
+  moj.Modules.SkipToContent = {
+    init: function() {
+      $('body').on('click', '.skip-link', function(e) {
+        e.preventDefault();
+
+        $($(e.target).attr('href'))
+          .attr('tabindex', -1)
+          .focus()
+          .blur(function() {
+            $(this).removeAttr('tabindex');
+          });
+      });
+    }
+  };
+}());
